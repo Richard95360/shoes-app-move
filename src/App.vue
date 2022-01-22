8af04f6a59c98c7fcadadb45465400c724bd4e60
Richard95360 <richard.haddad1@free.fr>
2022-01-22 13:33:27 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,113 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main id="app" @mousemove="mousemove">
+    <section class="products">
+        <Product 
+        v-for="product in products" 
+        :key="product.color" 
+        :product="product"
+        />
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from '@/components/Product.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Product
+  },
+  data() {
+    return {
+      products: [
+        {
+        title: 'Nike air Max',
+        color:'green',
+        bgText:'NIKE',
+        src:require('./assets/green-shoes.png')
+        },
+        {
+        title: 'Nike flex',
+        color:'blue',
+        bgText:'AIR',
+        src:require('./assets/blue-shoes.png')
+        },
+        {
+        title: 'Nike Roche Runs',
+        color:'pink',
+        bgText:'MAX',
+        src:require('./assets/pink-shoes.png')
+        }
+      ]
+    }
+  },
+  methods: {
+    mousemove(e){
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+  
+      let products = document.querySelectorAll('.products .product');
+
+      
+
+      for(let i= 0; i < products.length; i++){
+          let product = products[i];
+
+          let product_image = product.querySelector('.product-image-wrap');
+
+          let img_x = mouseX - this.coords(product_image).x;
+          let img_y = mouseY - this.coords(product_image).y;
+
+          
+          product_image.style.transform = `translateY(-${img_y/20}px) translateX(-${img_x/20}px)
+          translateZ(100px)`;
+
+          let bgText = product.querySelector('.bg-text');
+          let bg_x = mouseX - this.coords(bgText).x;
+          let bg_y = mouseY - this.coords(bgText).y;
+          bgText.style.transform = `translateX(${bg_x/25}px) translateY(${bg_y/25}px)`
+
+
+          
+      }
+    },
+    coords(el) {
+      let coords = el.getBoundingClientRect();
+
+      return {
+        x: coords.left / 2,
+        y: coords.top / 2
+
+       
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montseratt' sans-serif;
+}
+main {
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+  background-color: #EEE;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.products{
+  display: flex;
+  max-width: 1280px;
+  padding: 25px;
+  margin: 0 auto;
 }
 </style>
